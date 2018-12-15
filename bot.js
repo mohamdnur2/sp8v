f40ce60021243353cd9320291ca5af8a599bd69c
mohamdnur2 <44902522+mohamdnur2@users.noreply.github.com>
2018-12-15 10:38:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on('reconnecting', () => console.log('PROBOT credits miner is reconnectin
 
 
 client.on('message', message => {
-if(message.content.startsWith('1')) {
+if(message.content.startsWith('8')) {
 if(message.author.id !== "505678957752877056") return;
 var args = message.content.split(' ').slice(1).join(' ');
 message.channel.send(args);
